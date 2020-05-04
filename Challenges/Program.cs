dc5fc3f9f0cf1264c3775e7675bdaed0558b895a
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-04 14:21:23 -0500

    added string.join to get it to print out array

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -6,7 +6,23 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayReversed();
+        }
+
+        static void ArrayReversed()
+        {
+            int[] arr1 = { 3, 8, 5, 9, 12 };
+            int[] arr2 = new int[5];
+
+            int j = 0;
+
+            for (int i = arr1.Length - 1; i >= 0; i--)
+            {
+                arr2[j] = arr1[i];
+                j++;
+            }
+
+            Console.Write(string.Join(",", arr2));
         }
     }
 }
