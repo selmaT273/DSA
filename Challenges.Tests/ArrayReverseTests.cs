766c6f6b692abc66976f32dc93f6b04610397add
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-05 12:27:08 -0500

    added tests project and test for challenge 1 to confirm tests are set up correctly

---
+++ Challenges.Tests/ArrayReverseTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Challenges.Tests
+{
+    public class ArrayReverseTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.False(false);
+        }
+    }
+}
