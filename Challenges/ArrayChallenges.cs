c9fa24c9f692d2eb570d6883df818ac86433be70
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-06 10:26:45 -0500

    added comments for ArrayShift code

--- Challenges/ArrayChallenges.cs
+++ Challenges/ArrayChallenges.cs
@@ -22,26 +22,39 @@ namespace Challenges
 
         public static int[] InsertArrayShift(int[] inputArray, int num)
         {
+            // Create new empty array one index bigger than the input array
             int[] newArray = new int[inputArray.Length + 1];
 
+            // Finding middle of input and
+            // Using Math.Ceiling says if there's not an exact middle, look to the right
             decimal middle = Math.Ceiling(inputArray.Length / 2m);
 
+            // Loop through newArray 
             for (int i = 0; i < newArray.Length; i++)
             {
+                // If below the middle 
                 if (i < middle)
                 {
+                    // Set current index of newArray equal to value of inputArray at same index
                     newArray[i] = inputArray[i];
                 }
+
+                // If we're on the middle index
                 else if (i == middle)
                 {
+                    // Set current index of newArray equal to num that was passed in
                     newArray[i] = num;
                 }
+
+                // If we're past the middle index
                 else
                 {
+                    // Set current index of newArray equal to inputArray's value at the index 1 below
                     newArray[i] = inputArray[i - 1];
                 }
             }
 
+            // Return the new array with the num in the middle
             return newArray;
         }
     }
