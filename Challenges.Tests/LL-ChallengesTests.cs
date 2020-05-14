c07d39049124513290862eecc0be1b64f941494e
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-14 00:11:32 -0500

    list 2 is shorter test passing

--- Challenges.Tests/LL-ChallengesTests.cs
+++ Challenges.Tests/LL-ChallengesTests.cs
@@ -24,5 +24,22 @@ namespace Challenges.Tests
 
             Assert.Equal("{ 4 } -> { 1 } -> { 5 } -> { 2 } -> { 6 } -> { 3 } -> NULL", LL_Challenges.mergeLists(list1, list2).ToString());
         }
+
+        [Fact]
+        public void Merge2ListsDifferentLengthTest()
+        {
+            LinkedList list1 = new LinkedList();
+
+            list1.Append(4);
+            list1.Append(5);
+            list1.Append(6);
+
+            LinkedList list2 = new LinkedList();
+
+            list2.Append(7);
+            list2.Append(9);
+
+            Assert.Equal("{ 4 } -> { 7 } -> { 5 } -> { 9 } -> { 6 } -> NULL", LL_Challenges.mergeLists(list1, list2).ToString());
+        }
     }
 }
