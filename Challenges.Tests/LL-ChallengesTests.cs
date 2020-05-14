045ce8e2d3dd695882ff444d714e8de0377259e1
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-13 23:59:37 -0500

    merge lists method created and test passing for lists of same length

--- Challenges.Tests/LL-ChallengesTests.cs
+++ Challenges.Tests/LL-ChallengesTests.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using DataStructures;
+using Challenges;
+using Xunit;
+
 namespace Challenges.Tests
 {
     public class LL_ChallengesTests
     {
-        public LL_ChallengesTests()
+        [Fact]
+        public void Merge2ListsSameLengthTest()
         {
+            LinkedList list1 = new LinkedList();
+
+            list1.Append(4);
+            list1.Append(5);
+            list1.Append(6);
+
+            LinkedList list2 = new LinkedList();
+
+            list2.Append(1);
+            list2.Append(2);
+            list2.Append(3);
+
+            Assert.Equal("{ 4 } -> { 1 } -> { 5 } -> { 2 } -> { 6 } -> { 3 } -> NULL", LL_Challenges.mergeLists(list1, list2).ToString());
         }
     }
 }
--- Challenges/LL-Challenges.cs
+++ Challenges/LL-Challenges.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using DataStructures;
+
 namespace Challenges
 {
     public class LL_Challenges
     {
-        public LL_Challenges()
+        public static LinkedList mergeLists(LinkedList list1, LinkedList list2)
         {
+            Node current1 = list1.Head;
+            Node current2 = list2.Head;
+
+            while(current1 != null)
+            {
+                list1.InsertAfter(current1.Data, current2.Data);
+                current1 = current1.Next.Next;
+                current2 = current2.Next;
+            }
+
+            return list1;
         }
     }
 }
