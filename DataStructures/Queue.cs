e948f168311a6af69cbbf563f88ff780841da7ba
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-18 00:57:43 -0500

    added queue and tests with all passing

---
+++ DataStructures/Queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace DataStructures.Queue
+{
+    public class Queue<T>
+    {
+        private Node front;
+        private Node rear;
+
+        public void Enqueue(T value)
+        {
+            if (rear == null)
+            {
+                rear = new Node(value);
+                front = rear;
+            }
+            else
+            {
+                Node newValue = new Node(value);
+                rear.Next = newValue;
+                rear = newValue;
+            }
+
+        }
+
+        public T Dequeue()
+        {
+            if (front == null)
+            {
+                throw new ArgumentException();
+            }
+
+            Node frontValue = front;
+            front = front.Next;
+
+            return frontValue.Value;
+        }
+
+        public T Peek()
+        {
+            if (front == null)
+            {
+                throw new ArgumentException();
+            }
+
+            return front.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            if (front == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public class Node
+        {
+            public Node Next { get; set; }
+            public T Value { get; set; }
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+    }
+}
---
+++ DataStructuresTests/QueueTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using DataStructures.Queue;
+using Xunit;
+
+namespace DataStructuresTests
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void PeekEmptyQueueThrowsTest()
+        {
+            Queue<object> queue = new Queue<object>();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                queue.Peek();
+            });
+        }
+
+        [Fact]
+        public void QueueIsEmptyReturnsTrueIfEmptyTest()
+        {
+            Queue<object> queue = new Queue<object>();
+
+            Assert.True(queue.IsEmpty());
+        }
+
+        [Fact]
+        public void CanEnqueueTest()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(5);
+
+            Assert.Equal(5, queue.Peek());
+
+
+        }
+
+        [Fact]
+        public void CanEnqueueMultipleValuesTest()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(3);
+            queue.Enqueue(5);
+            queue.Enqueue(8);
+
+            Assert.Equal(3, queue.Peek());
+        }
+
+        [Fact]
+        public void CanDequeueOneTest()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(3);
+            queue.Enqueue(5);
+            queue.Enqueue(8);
+
+            Assert.Equal(3, queue.Dequeue());
+        }
+
+        [Fact]
+        public void CanDequeueUntilEmptyTest()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(3);
+            queue.Enqueue(5);
+            queue.Enqueue(8);
+
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+
+            Assert.True(queue.IsEmpty());
+        }
+
+        [Fact]
+        public void DequeueEmptyStackThrowsTest()
+        {
+            Queue<object> queue = new Queue<object>();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                queue.Dequeue();
+            });
+        }
+    }
+}
