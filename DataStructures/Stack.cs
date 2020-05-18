766efba521916307976c86fd18f09b7e31d6cfad
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-17 23:06:31 -0500

    set up stack and have throws exception if empty test passing

---
+++ DataStructures/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DataStructures
+{
+    public class Stack<T>
+    {
+        private Node top;
+
+        public void Push(T value)
+        {
+
+        }
+
+        public T Pop()
+        {
+            return default;
+        }
+
+        public T Peek()
+        {
+            if (top == null)
+            {
+                throw new ArgumentException();
+            }
+
+            return default;
+        }
+
+        private class Node
+        {
+            public Node Next { get; set; }
+            public T Value { get; set; }
+        }
+    }
+}
---
+++ DataStructuresTests/StackTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DataStructures;
+using Xunit;
+
+namespace DataStructuresTests
+{
+    public class StackTests
+    {
+        [Fact]
+        public void PeekEmptyStackThrows()
+        {
+            Stack<object> stack = new Stack<object>();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                stack.Peek();
+            });
+        }
+    }
+}
