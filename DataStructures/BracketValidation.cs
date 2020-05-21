f2a8e0fe2e2079ab100680cd9afa8148282def9a
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-20 23:10:52 -0500

    happy path test is passing

---
+++ DataStructures/BracketValidation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DataStructures.Stack;
+
+namespace DataStructures
+{
+    public class BracketValidation
+    {
+        public bool MultiBracketValidation(string BracketString)
+        {
+            bool error = false;
+            Stack<char> stack = new Stack<char>();
+            foreach (var item in BracketString.ToCharArray())
+            {
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    stack.Push(item);
+                }
+                else if (item == ')' || item == '}' || item == ']')
+                {
+                    if (stack.Peek() != GetComplementBracket(item))
+                    {
+                        error = true;
+                        break;
+                    }
+                }
+            }
+
+            if (error)
+                return false;
+            else
+                return true;
+        }
+
+        private static char GetComplementBracket(char item)
+        {
+            return item switch
+            {
+                ')' => '(',
+                '}' => '{',
+                ']' => '[',
+                _ => ' ',
+            };
+        }
+    }
+}
---
+++ DataStructuresTests/BracketValidationTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DataStructures;
+using DataStructures.Stack;
+using Xunit;
+
+namespace DataStructuresTests
+{
+    public class BracketValidationTests
+    {
+        [Fact]
+        public void CanValidateBalancedStringTest()
+        {
+            BracketValidation bracketValidation = new BracketValidation();
+
+            Assert.True(bracketValidation.MultiBracketValidation("(){}[[]]"));
+        }
+    }
+}
