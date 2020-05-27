bb354aa2ede489c7b9c8442601a7e2dc249f0675
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-27 00:22:58 -0500

    binary search tree can add multiple nodes

--- DataStructures/Trees/BinarySearchTree.cs
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -3,19 +3,50 @@
 namespace DataStructures.Trees
 {
 
-    public class BinarySearchTree
+    public class BinarySearchTree : BinaryTree
     {
-        public Node Root { get; set; }
-        public BinarySearchTree()
-        {
-            Root = null;
-        }
-
         public void Add(int value)
         {
-            if (Root == null)
+            if(Root == null)
             {
                 Root = new Node(value);
+            }
+            else
+            {
+                Add(Root, value);
+            }
+        }
+
+        private void Add(Node node, int value)
+        { 
+            if (node == null)
+            {
+                node = new Node(value);
+            }
+            else
+            {
+                if(value < node.Value)
+                {
+                    if (node.Left == null)
+                    {
+                        node.Left = new Node(value);
+                    }
+                    else
+                    {
+                        Add(node.Left, value);
+                    }
+                }
+                else
+                {
+                    if(node.Right == null)
+                    {
+                        node.Right = new Node(value);
+                    }
+                    else
+                    {
+                        Add(node.Right, value);
+                    }
+                }
             }
         }
 
