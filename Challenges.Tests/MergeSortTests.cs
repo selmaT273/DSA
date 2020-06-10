8c7197bab47e48cd428f902215c50702c6a3b718
Stacey Teltser <stacey.teltser@gmail.com>
2020-06-09 22:17:00 -0500

    merge sort happy path test passing

---
+++ Challenges.Tests/MergeSortTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Challenges.Tests
+{
+    public class MergeSortTests
+    {
+        [Fact]
+        public void CanImplementMergeSortTest()
+        {
+            int[] testArray = new int[] { 8, 4, 23, 42, 16, 15 };
+
+            int[] result = MergeSort.MergeSorting(testArray);
+
+            Assert.Equal(new[] { 4, 8, 15, 16, 23, 42 }, result);
+        }
+    }
+}
