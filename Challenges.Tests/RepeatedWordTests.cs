ef06e393b520de03d2bf1e36cc79374d63c47a80
Stacey Teltser <stacey.teltser@gmail.com>
2020-06-15 17:05:44 -0500

    happy path test working without accounting for punctuation

---
+++ Challenges.Tests/RepeatedWordTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Challenges.Tests
+{
+    public class RepeatedWordTests
+    {
+        [Fact]
+        public void CanReturnRepeatedWordsTest()
+        {
+
+            string inputStr = "Once upon a time there was a brave princess who";
+
+            string outputStr = RepeatedWord.FirstRepeating(inputStr);
+
+            Assert.Equal("a", outputStr);
+            
+        }
+
+    }
+}
---
+++ Challenges/RepeatedWord.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class RepeatedWord
+    {
+        public static string FirstRepeating(string inputStr)
+        {
+            string[] words = inputStr.Split(' ');
+
+            Dictionary<string, int> d = new Dictionary<string, int>();
+
+            for (int i = 0; i <= words.Length - 1; i++)
+            {
+                string s = words[i];
+
+                if (d.ContainsKey(s))
+                {
+                    return s;
+                }
+                else
+                {
+                    d.Add(s, 1);
+                }
+            }
+
+            return null; 
+        }
+        
+
+        //public string FindRepeatedWord(string inputString)
+        //{
+        //    string inputString;
+        //    var dictionaryTracker = new Dictionary<string, bool>();
+        //    StringBuilder outputString = new StringBuilder();
+        //    string[] eachWord = inputString.Split(new char[] { ' ', ',', ';', '.' },
+        //        StringSplitOptions.RemoveEmptyEntries);
+
+        //    foreach (string current in eachWord)
+        //    {
+        //        string lowerCase = current.ToLower();
+
+        //        if (dictionaryTracker.ContainsKey(lowerCase))
+        //        {
+        //            outputString.Append(current);
+        //            dictionaryTracker.Add(lowerCase, true);
+        //        }
+
+        //    }
+
+        //    return outputString.ToString();
+        //}
+    }
+}
