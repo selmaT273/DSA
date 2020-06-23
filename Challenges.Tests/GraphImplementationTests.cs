b9c5ed583d0efbb3468a7958bc8d8d278573b4ff
Stacey Teltser <stacey.teltser@gmail.com>
2020-06-22 23:00:16 -0500

    graph implemented with 2 tests passing

--- Challenges.Tests/GraphImplementationTests.cs
+++ Challenges.Tests/GraphImplementationTests.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using Challenges;
+using Xunit;
+
 namespace Challenges.Tests
 {
     public class GraphImplementationTests
     {
-        // Stuff goes here
+        [Fact]
+        public void GraphStartsEmptyTest()
+        {
+            var graph = new Graph<int>();
+
+            Assert.Empty(graph.GetNodes());
+            Assert.Equal(0, graph.Size());
+        }
+
+        [Fact]
+        public void CanAddNodeTest()
+        {
+            var graph = new Graph<int>();
+
+            var node = graph.AddNode(4);
+
+            Assert.Equal(1, graph.Size());
+            Assert.NotNull(node);
+            Assert.Equal(4, node.Value);
+            Assert.Contains(node, graph.GetNodes());
+        }
     }
 }
--- Challenges/GraphImplementation.cs
+++ Challenges/GraphImplementation.cs
@@ -1,10 +1,50 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace Challenges
 {
-    public class GraphImplementation
+
+    // Node AddNode(T value);
+
+    // void AddEdge(Node node1, Node node2);
+
+    // void AddEdge(Node node1, Node node2, int? weight = null); <-- can add weight in later
+
+    // IReadOnlyList<Node> GetNodes();
+
+    // IEnumerable<Node> GetNeighbors(Node node) => node.Neighbors;
+
+    // int Size();
+
+    // class Node { ... }
+
+    
+
+    public class Graph<T>
     {
-        public GraphImplementation()
+        readonly List<Node> nodes = new List<Node>();
+
+        public Node AddNode(T value)
         {
+            var node = new Node { Value = value };
+            nodes.Add(node);
+            return node;
         }
+
+        public class Node
+        {
+            public T Value { get; set; }
+
+
+        }
+
+        public IReadOnlyList<Node> GetNodes()
+        {
+            return nodes;
+        }
+
+        public int Size() => nodes.Count;
     }
 }
